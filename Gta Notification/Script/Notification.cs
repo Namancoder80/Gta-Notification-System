1f3fa477fe13df0fcd611a16a06250bf0c75386f
Naman Gulati <96739382+Namancoder80@users.noreply.github.com>
2023-11-25 16:43:33 +0530

    Add files via upload

---
+++ Gta Notification/Script/Notification.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace TeamFlyer.Message
+{
+    public class Notification : MonoBehaviour
+    {
+        [SerializeField] private GameObject NotificationPrefab;
+        [SerializeField] private GameObject NotificationParent;
+        private List<GameObject> SpawnNotification = new List<GameObject>();
+        public void ShowNotification(string Message)
+        {
+            StartCoroutine(CreateNotification(Message));
+        }
+        private IEnumerator CreateNotification(string Subtitle)
+        {
+            GameObject _Notification = Instantiate(NotificationPrefab, NotificationParent.transform);
+            SpawnNotification.Add(_Notification);
+
+            for (int i = 0; i < SpawnNotification.Count; i++)
+            {
+                GameObject popup = SpawnNotification[i];
+                TMP_Text[] TextHolder = popup.GetComponentsInChildren<TMP_Text>();
+                if (TextHolder != null)
+                {
+                    TMP_Text _Subtitle = TextHolder[1];
+                    _Subtitle.text = Subtitle;
+                }
+            }
+            yield return new WaitForSeconds(6f);
+
+            for (int i = 0; i < SpawnNotification.Count; i++)
+            {
+                GameObject popup = SpawnNotification[i];
+                CanvasGroup canvasGroup = popup.GetComponent<CanvasGroup>();
+                if (canvasGroup != null)
+                {
+                    canvasGroup.LeanAlpha(0, 1.5f);
+                }
+            }
+            yield return new WaitForSeconds(2f);
+
+            for (int i = 0; i < SpawnNotification.Count; i++)
+            {
+                Destroy(SpawnNotification[i]);
+                SpawnNotification.RemoveAt(i);
+            }
+
+        }
+    }
+}
